92c38da3b7a2cbcb2de40887b7b3ab5d1dcbed0a
sejin8839 <sejinon01@gmail.com>
2023-03-15 10:06:25 +0900

    Update 0315.cpp

--- 0315.cpp
+++ 0315.cpp
@@ -1,35 +1,16 @@
 
-
-#include<iostream>
-#include<cmath>
-#include<iomanip>
-
-using namespace std;
-
-
-if (int num =13; num % 2 == 0)
-{
-    cout <<num << " is an even number\n";
-}
-else
-{
-    cout << num << " is an odd number\n";
-}
+#include <iostram>
+using namespaace std;
 
 int main()
 {
-int num, val = 12;
-cout << val++; // displays 12, 
-// val is now 13;
-cout << ++val; // sets val to 14,
-// then displays it
-num = --val; // sets val to 13,
-// stores 13 in num
-num = val--; // stores 13 in num,
-// sets val to 12
+    int number = 1,;
 
-return 0;
-
-]
-
-
+    while (number <= 5)
+    {
+        cout <<"Hello\n";
+        number++;
+    }
+    cout << "That's all!\n"
+    return 0;
+}
