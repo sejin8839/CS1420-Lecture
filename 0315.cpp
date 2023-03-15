a5ca49a004b6be89ec9ac9b59893009a94f35dd7
sejin8839 <sejinon01@gmail.com>
2023-03-15 09:55:41 +0900

    Update 0315.cpp

--- 0315.cpp
+++ 0315.cpp
@@ -16,4 +16,20 @@ else
     cout << num << " is an odd number\n";
 }
 
+int main()
+{
+int num, val = 12;
+cout << val++; // displays 12, 
+// val is now 13;
+cout << ++val; // sets val to 14,
+// then displays it
+num = --val; // sets val to 13,
+// stores 13 in num
+num = val--; // stores 13 in num,
+// sets val to 12
+
+return 0;
+
+]
+
 
