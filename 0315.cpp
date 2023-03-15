e9a0b5d9337bf1cbfbb65d600b6530764d7bda39
sejin8839 <sejinon01@gmail.com>
2023-03-15 09:46:03 +0900

    Update 0315.cpp

---
+++ 0315.cpp
@@ -0,0 +1,19 @@
+
+
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+
+
+if (int num =13; num % 2 == 0)
+{
+    cout <<num << " is an even number\n";
+}
+else
+{
+    cout << num << " is an odd number\n";
+}
+
+
